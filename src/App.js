2f8760aa00d8f807f0fe38c8e0ae2170114af0db
RutujTailor28 <tailorrutuj28@gmail.com>
2019-08-14 17:59:43 +0545

    Conditional Statements Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Button} from 'reactstrap';
+
 import {Person} from './Person/Person';
 
 class App extends Component {
@@ -9,39 +11,76 @@ class App extends Component {
             {name: 'Manu', age: 29},
             {name: 'Stephanie', age: 26}
         ],
-        otherState: 'some other value'
+        otherState: 'some other value',
+        personShow: false
     }
+
 
     switchNameHandler = (newName) => {
         // console.log('Was clicked!');
         // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
         this.setState({
             persons: [
-                {name: 'HEY', age: 28},
+                {name: newName, age: 28},
                 {name: 'RICk', age: 29},
                 {name: 'WASSUP', age: 27}
             ]
         })
     }
-
-
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: event.target.value, age: 28},
+                {name: 'RUTUJ', age: 29},
+                {name: 'HEY', age: 27}
+            ]
+        })
+    }
+    toggleListHandler = () => {
+        const doesShow = this.state.personShow;
+        this.setState({personShow: !doesShow});
+    }
+    deleteHandler = (indexOfPerson) => {
+        const persons = this.state.persons;
+        persons.splice(indexOfPerson, 1);
+        this.setState({persons: persons});
+    }
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            borderRadius: '6px',
+            boxShadow: '0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2)',
+            padding: '8px',
+            cursor: 'pointer'
+        }
+        let person = null;
+        if (this.state.personShow) {
+            person = (
+                <div>
+                    {
+                        this.state.persons.map((person, index) => {
+                            return <Person
+                                age={person.age}
+                                name={person.name}
+                                key={"person"+index}
+                                click={() => this.deleteHandler(index)}
+                            />
+                        })
+                    }
+                </div>
+            );
+        }
+
         return (
             <div className="App">
+
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}/>
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler}>My Hobbies: Racing</Person>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}/>
+                <Button style={style} onClick={this.toggleListHandler}>Switch Name</Button>
+                {person}
+
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import   React from 'react'
+import './Person.css'
 export const Person = (props) => {
     return (
-        <div>
-            <p onClick={props.click}>Hey Person i am with someone who counting on me is {props.name} and his age is {props.age}</p>
-            <p>{props.children}</p>
-        </div>
+            <div className="dash__card">
+                    <p onClick={props.click}>Hey Person i am with someone who counting on me is {props.name} and his age
+                        is {props.age}</p>
+                    <p>{props.children}</p>
+                    <input type='text' onChange={props.change} />
+            </div>
+
     );
 }
 
